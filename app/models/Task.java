161bc4388924f3a9ec6c3066756b2e1fefc520fb
Andrej Soltes <andrey.dfk@gmail.com>
2012-11-22 20:19:25 +0100

    trochu upraveny model

--- app/models/Task.java
+++ app/models/Task.java
@@ -17,4 +17,13 @@ public class Task extends Model {
 	@ManyToMany
 	public List<User> assigned;
 	
+	public boolean completed;
+	
+	public Task(String name, Date from, Date to, boolean completed) {
+		this.name = name;
+		this.from = from;
+		this.to = to;
+		this.completed = completed;
+	}
+	
 }
