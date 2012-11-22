6fd9cc75ec084e1aad08fe92ecc6bbd672e69259
unknown <ejo@ejo-PC.(none)>
2012-11-22 17:26:46 +0100

    user, CRUD, Secure

---
+++ app/controllers/Users.java
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+ 
+import play.*;
+import play.mvc.*;
+
+@With(Secure.class)
+public class Users extends CRUD {    
+}
