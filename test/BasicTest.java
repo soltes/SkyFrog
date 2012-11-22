80c9e3a74249585fa379c43f83d8c149449c4510
unknown <ejo@ejo-PC.(none)>
2012-11-22 17:00:41 +0100

    moj prvy commit

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -6,8 +6,16 @@ import models.*;
 public class BasicTest extends UnitTest {
 
     @Test
-    public void aVeryImportantThingToTest() {
-        assertEquals(2, 1 + 1);
-    }
+    public void createAndRetrieveUser() {
+    // Create a new user and save it
+    new User("erik@gmail.com", "secret", "Erik").save();
+    
+    // Retrieve the user with e-mail address bob@gmail.com
+    User erik = User.find("byEmail", "erik@gmail.com").first();
+    
+    // Test 
+    assertNotNull(erik);
+    assertEquals("Erik", erik.fullname);
+}
 
 }
