7a68050674e716a52bf6c006a77031003961ff18
Andrej Soltes <andrey.dfk@gmail.com>
2012-11-22 17:09:16 +0100

    navigacia, vytvor. projektu

---
+++ app/controllers/Project.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+
+public class Project extends Controller {
+
+    public static void createNew() {
+        render();
+    }    
+
+    public static void createNew2() {
+        render();
+    }
+
+    
+}
