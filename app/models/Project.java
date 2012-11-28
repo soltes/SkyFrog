dbadd680752f278d94ca90ca378ea5543a1e2de7
ejo <ejo81@centrum.sk>
2012-11-28 22:32:53 +0100

    index_logged

--- app/models/Project.java
+++ app/models/Project.java
@@ -16,7 +16,7 @@ public class Project extends Model {
 	
 	public String name;
 	public String desc;
-    public String giturl;
+        public String giturl;
 	
 	@ManyToMany
 	public List<User> users;
